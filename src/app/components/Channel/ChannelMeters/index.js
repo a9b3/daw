0fd5cb9050a4a793b93e58a39a3764318b49997f
Sam Lau <esayemm@gmail.com>
2018-02-07 01:41:46 -0800

    add arrow indicator to meter

--- src/app/components/Channel/ChannelMeters/index.js
+++ src/app/components/Channel/ChannelMeters/index.js
@@ -1,5 +1,6 @@
 import styles          from './index.scss'
 import cx              from 'classnames'
+import { observer }    from 'mobx-react'
 import PropTypes       from 'prop-types'
 import React           from 'react'
 
@@ -8,13 +9,38 @@ import Meter           from 'components/Meter'
 import withChannelData from 'components/Channel/withChannelData'
 
 @withChannelData
+@observer
 export default class ChannelMeters extends React.Component {
-  static propTypes = { channelData: PropTypes.object }
+  static propTypes = {
+    channelData: PropTypes.object,
+    channel: PropTypes.object,
+  }
+
+  _el = null
+
+  handleClick = event => {
+    const { channel } = this.props
+    const diff =
+      1 - (event.clientY - this._el.offsetTop) / this._el.offsetHeight
+    channel.setGain(diff)
+  }
 
   render() {
-    const { channelData, ...rest } = this.props
+    const { channel, channelData, ...rest } = this.props
     return (
-      <div {...rest} className={cx(styles.meters, rest.className)}>
+      <div
+        {...rest}
+        className={cx(styles.meters, rest.className)}
+        onClick={this.handleClick}
+        ref={el => (this._el = el)}
+      >
+        <div
+          className={styles.arrow}
+          style={{
+            transform: `translateY(${this._el &&
+              this._el.offsetHeight - this._el.offsetHeight * channel.gain}px)`,
+          }}
+        />
         <Meter
           className={styles.meter}
           peak={channelData.left.peak}
