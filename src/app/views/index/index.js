6478fbbcbcf07dfc41ac8d3bcc1981ed90de768f
Sam Lau <esayemm@gmail.com>
2018-02-06 18:24:42 -0800

    update callsite

--- src/app/views/index/index.js
+++ src/app/views/index/index.js
@@ -1,28 +1,19 @@
 import styles               from './styles.scss'
 import { inject, observer } from 'mobx-react'
+import PropTypes            from 'prop-types'
 import React                from 'react'
 
 import { Mixer, Sound }     from 'app/mixer'
 
 import Channel              from 'components/Channel'
 
-// const sound = new Sound()
-// async function foo() {
-//   const mixer = new Mixer()
-//   const channel = mixer.addChannel()
-//
-//   sound.output.connect(channel.input)
-//   await sound.load(
-//     require('../../../../../../../Desktop/documents/Music/Download/Maria Takeuchi 竹内 まりや Plastic Love-3bNITQR4Uso.mp3'),
-//   )
-//   sound.play()
-// }
-//
-// foo()
-
 @inject('mixer')
 @observer
 export default class Index extends React.Component {
+  static propTypes = {
+    mixer: PropTypes.object.isRequired,
+  }
+
   async componentDidMount() {
     const { mixer } = this.props
     const sound = new Sound()
@@ -31,12 +22,6 @@ export default class Index extends React.Component {
       require('../../../../../../../Desktop/documents/Music/Download/Maria Takeuchi 竹内 まりや Plastic Love-3bNITQR4Uso.mp3'),
     )
     sound.play()
-
-    const cb = () => {
-      this.setState({})
-      window.requestAnimationFrame(cb)
-    }
-    window.requestAnimationFrame(cb)
   }
 
   render() {
@@ -44,23 +29,11 @@ export default class Index extends React.Component {
     return (
       <div className={styles.index}>
         {mixer.channels.map((channel, i) => {
-          const [left, right] = channel.analyser.getDecibel()
           return (
             <Channel
               key={i}
               style={{ width: 100, height: 320 }}
-              panPosition={channel.panPosition}
-              channelIndex={i}
-              meterData={{
-                left: {
-                  main: left,
-                  peak: channel.rms.peaks[0] * 100,
-                },
-                right: {
-                  main: right,
-                  peak: channel.rms.peaks[1] * 100,
-                },
-              }}
+              channel={channel}
             />
           )
         })}
@@ -68,18 +41,7 @@ export default class Index extends React.Component {
         <Channel
           key={2}
           style={{ width: 100, height: 320 }}
-          panPosition={mixer.master.panPosition}
-          channelIndex={2}
-          meterData={{
-            left: {
-              main: mixer.master.rms.rms[0] * 100,
-              peak: mixer.master.rms.peaks[0] * 100,
-            },
-            right: {
-              main: mixer.master.rms.rms[1] * 100,
-              peak: mixer.master.rms.peaks[1] * 100,
-            },
-          }}
+          channel={mixer.master}
         />
       </div>
     )
