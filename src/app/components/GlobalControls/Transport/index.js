c109bf013a2109456e61c14f5f86edce860df672
Sam Lau <esayemm@gmail.com>
2018-02-08 00:44:24 -0800

    add Transport component

--- src/app/components/GlobalControls/Transport/index.js
+++ src/app/components/GlobalControls/Transport/index.js
@@ -1,15 +1,26 @@
-import styles                 from './index.scss'
-import PropTypes              from 'prop-types'
-import React                  from 'react'
+import styles from './index.scss'
+import cx from 'classnames'
+import PropTypes from 'prop-types'
+import React from 'react'
 
 import ControlIcon, { TYPES } from 'components/ControlIcon'
 
 export default function Transport() {
   return (
     <section className={styles.transport}>
-      <ControlIcon type={TYPES.play} />
-      <ControlIcon type={TYPES.stop} />
-      <ControlIcon type={TYPES.record} />
+      <div className={styles.transport__item}>{`3 . 12 . 1`}</div>
+      <ControlIcon
+        className={cx(styles.icon, styles.transport__item)}
+        type={TYPES.play}
+      />
+      <ControlIcon
+        className={cx(styles.icon, styles.transport__item)}
+        type={TYPES.stop}
+      />
+      <ControlIcon
+        className={cx(styles.icon, styles.transport__item)}
+        type={TYPES.record}
+      />
     </section>
   )
 }
