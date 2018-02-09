ec04e9c058c06b566dbf369fde514295995ae0a2
Sam Lau <esayemm@gmail.com>
2018-02-09 01:13:59 -0800

    add Virtualized List component

--- src/app/components/VirtualizedLayouts/List/_index.stories.js
+++ src/app/components/VirtualizedLayouts/List/_index.stories.js
@@ -1,15 +1,29 @@
-import { withKnobs, text } from '@storybook/addon-knobs'
-import { storiesOf }       from '@storybook/react'
-import React               from 'react'
+import { withKnobs, number } from '@storybook/addon-knobs'
+import { storiesOf }         from '@storybook/react'
+import React                 from 'react'
 
-import List                from './index.js'
+import List                  from './index.js'
 
 const renderRow = i => {
-  return <div style={{ width: '5em', height: '5em' }}>{i}</div>
+  return (
+    <div
+      style={{
+        border: '1px solid black',
+        width: '10em',
+        height: `100px`,
+      }}
+    >
+      {i}
+    </div>
+  )
 }
 
 storiesOf('VirtualizedLayouts/List', module)
   .addDecorator(withKnobs)
   .add('default', () => {
-    return <List renderRow={renderRow} count={1000} />
+    return (
+      <div style={{ width: '100%', height: '500px' }}>
+        <List renderRow={renderRow} total={number('total', 100000)} />
+      </div>
+    )
   })
--- src/app/components/VirtualizedLayouts/List/index.js
+++ src/app/components/VirtualizedLayouts/List/index.js
@@ -1,23 +1,117 @@
-import { times } from 'lodash'
 import PropTypes from 'prop-types'
 import React     from 'react'
 
 export default class List extends React.PureComponent {
   static propTypes = {
-    count: PropTypes.number,
+    renderRow: PropTypes.func,
+    total: PropTypes.number,
+    buffer: PropTypes.number,
+  }
+
+  static defaultProps = {
+    buffer: 5,
   }
 
   state = {
-    ready: false,
+    range: [0, 10],
+    spacers: {
+      top: 0,
+      bottom: 1000,
+    },
+    averageRowHeight: 100,
   }
 
   componentDidMount() {
-    this.setState({ ready: true })
+    // hydrate state
+    this._updateState()
+  }
+
+  /* --------------- DOM Refs --------------- */
+  _viewBoxDOM = undefined
+  _setViewBoxDOM = el => {
+    this._viewBoxDOM = el
+  }
+
+  /* --------------- Helpers --------------- */
+
+  _renderRange = range => {
+    const { renderRow } = this.props
+    const nodes = []
+    for (let i = range[0]; i < range[1]; i++) {
+      nodes.push(renderRow(i))
+    }
+    return nodes
+  }
+
+  _calcRangeInViewBox = (
+    scrollTop,
+    viewBoxHeight,
+    buffer,
+    total,
+    averageRowHeight,
+  ) => {
+    const estimatedTopRows = Math.round(scrollTop / averageRowHeight)
+    const estimatedViewBoxRows = Math.round(viewBoxHeight / averageRowHeight)
+    return [
+      estimatedTopRows - buffer > 0 ? estimatedTopRows - buffer : 0,
+      estimatedTopRows + estimatedViewBoxRows + buffer < total
+        ? estimatedTopRows + estimatedViewBoxRows + buffer
+        : total,
+    ]
+  }
+
+  _updateState = (scrollTop = 0) => {
+    const { total, buffer } = this.props
+    const { averageRowHeight } = this.state
+    const range = this._calcRangeInViewBox(
+      scrollTop,
+      this._viewBoxDOM.offsetHeight,
+      buffer,
+      total,
+      averageRowHeight,
+    )
+    const spacers = {
+      top: Math.abs(0 - range[0]) * averageRowHeight,
+      bottom: Math.abs(total - range[1]) * averageRowHeight,
+    }
+
+    this.setState({ range, spacers })
+  }
+
+  handleScroll = event => {
+    this._updateState(event.target.scrollTop)
   }
 
   render() {
-    const { count, renderRow } = this.props
-    const { ready } = this.state
-    return <div>{!ready ? renderRow(0) : times(count, renderRow)}</div>
+    const { range, spacers } = this.state
+
+    return (
+      <div
+        style={{
+          width: '100%',
+          height: '100%',
+          overflow: 'auto',
+        }}
+        onScroll={this.handleScroll}
+        ref={this._setViewBoxDOM}
+      >
+        <div
+          style={{
+            paddingTop: spacers.top,
+            position: 'relative',
+            paddingBottom: spacers.bottom,
+          }}
+        >
+          <div
+            style={{
+              position: 'absolute',
+              top: range[0] * 100,
+            }}
+          >
+            {this._renderRange(range)}
+          </div>
+        </div>
+      </div>
+    )
   }
 }
