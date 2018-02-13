32e2b0bdfe653f6363f4a6e7c72ba1bb9ac6cc27
Sam Lau <esayemm@gmail.com>
2018-02-12 22:33:26 -0800

    [package] use faker for demo data

--- src/app/mobx/index.js
+++ src/app/mobx/index.js
@@ -1,28 +1,36 @@
-import { Sequencer }          from 'app/mixer'
+import faker                  from 'faker'
 import { action, observable } from 'mobx'
+
+import { Sequencer }          from 'app/mixer'
 
 export default {
   sequencer: new Sequencer({
     tracks: [
       {
-        label: 'SAMPLE',
+        label: faker.name.firstName(),
         clips: { 0: { label: 'intro' }, 3: { label: 'chorus' } },
       },
       {
-        label: 'SAMPLE',
+        label: faker.name.firstName(),
         clips: {
           1: { label: 'a' },
           2: { label: 'b' },
           3: { label: 'c' },
         },
       },
-      {},
       {
-        label: 'SAMPLE',
+        label: faker.name.firstName(),
+      },
+      {
+        label: faker.name.firstName(),
         clips: { 0: { label: 'voice' }, 5: { label: 'bg' } },
       },
-      {},
-      {},
+      {
+        label: faker.name.firstName(),
+      },
+      {
+        label: faker.name.firstName(),
+      },
     ],
     sends: [
       {
