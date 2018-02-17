c64d43d5c9a0f36a3ab137432d833b56aaeefb1b
Sam Lau <esayemm@gmail.com>
2018-02-16 21:27:33 -0800

    [utils] add documentation to intervalWorker

--- src/app/utils/intervalWorker/index.js
+++ src/app/utils/intervalWorker/index.js
@@ -3,6 +3,14 @@ import IntervalWorker  from 'worker-loader!./IntervalWorker.js'
 
 const worker = new IntervalWorker()
 
+/**
+ * Uses web workers to trigger intervals. Useful for more intervals that need to
+ * happen when user is not focused on the current tab.
+ *
+ * ex:
+ * const cancelInterval = await setInterval(() => {}, 100)
+ * cancelInterval()
+ */
 export async function setInterval(cb, ms) {
   const id = await postMessage(worker, { type: 'setInterval', arg: { ms } })
   worker.onmessage = ({ data }) => {
