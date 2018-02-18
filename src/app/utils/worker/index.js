f314c9a2b667cd101697805519cba51226fe0953
Sam Lau <esayemm@gmail.com>
2018-02-17 16:07:12 -0800

    [utils] add documentation to worker

--- src/app/utils/worker/index.js
+++ src/app/utils/worker/index.js
@@ -1,5 +1,12 @@
 import uuid from 'uuid/v4'
 
+/**
+ * postMessage is a helper function for interfacing with web workers.
+ *
+ * @param {WebWorker} worker
+ * @param {object} arg
+ * @returns {any}
+ */
 export function postMessage(worker, arg) {
   return new Promise(resolve => {
     const taskId = uuid()
